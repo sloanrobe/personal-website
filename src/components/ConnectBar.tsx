8c2a1023b7421167e5f599ad79bd53f6fcef6b13
Benji Sloan <sloanrobe@hotmail.com>
2025-09-21 12:56:00 +0200

    Created CTA

---
+++ src/components/ConnectBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ConnectBar() {
+  return (
+    <div className='connect-section' >
+        <h2>Let's connect to explore ways to elevate your sports technology company!</h2>
+    </div>
+  )
+}
+
+export default ConnectBar
