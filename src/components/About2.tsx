97ffffed1b90305b468266babf53d34a3f9e419f
Benji Sloan <sloanrobe@hotmail.com>
2025-09-21 12:55:13 +0200

    Removed - went with original About section

---
+++ src/components/About2.tsx
@@ -0,0 +1,28 @@
+function About2() {
+  return (
+    <div className='about-container2'>
+      <div className='about-section2'>
+        <h2>About Me</h2>
+        <p>One sentence about who I am.</p>
+        <p>Second sentence about what I've done.</p>
+        <p>Third sentence about what I am looking to do.</p>
+        <p>One sentence about who I am.</p>
+        <p>Second sentence about what I've done.</p>
+        <p>Third sentence about what I am looking to do.</p>
+        <p>One sentence about who I am.</p>
+        <p>Second sentence about what I've done.</p>
+        <p>Third sentence about what I am looking to do.</p>       
+      </div>
+      <div className='twoimages'>
+        <div className="img-wrapper">
+            <img src="public/images/NYJ.jpg" alt="" />
+        </div>
+        <div className="img-wrapper">
+            <img src="public/images/Speaker_5.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About2
