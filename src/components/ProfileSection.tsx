ecbafd740676690e04c2c67aaf952238ae38e13e
Benji Sloan <sloanrobe@hotmail.com>
2025-09-26 11:18:45 +0200

    Edited margins

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -3,13 +3,17 @@ import React from 'react'
 function ProfileSection() {
   return (
     <div className='profile-container'>
-      <div className='profile-photo'>
-        <img src="public/images/BenjiHeadshotNoBackground.png" alt="" />
-      </div>
+      {<div className='profile-photo'>
+        {<img src="public/images/BenjiHeadshotNoBackground.png" alt="" />}
+      </div>}
       <div className='headline'>
-          <p>Performance Visionary.</p>
+        <p className='headline-line'>Visionary Leadership.</p>
+        <p className='headline-line'>Strategic Insight.</p>
+        <p className='headline-line'>Proven Results.</p>
+        {<p className='headline-sub'>Where Performance Meets Sports Tech Innovation.</p>}
+          {/* <p>Performance Visionary.</p>
           <p>Sports Tech Strategist.</p>
-          <p>Collaborative Leader.</p>
+          <p>Collaborative Leader.</p> */}
       </div>
     </div>
   )
