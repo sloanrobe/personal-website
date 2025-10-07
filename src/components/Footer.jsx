5a0d5a5c8870bfa77e818e19bf3584b5ed82ca46
Benji Sloan <sloanrobe@hotmail.com>
2025-10-07 13:07:21 +0200

    Added simple footer copyright language

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+        <p>©2025 Benji Sloan.  All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
