e16715304354b328d41db308fc31b860da3a559e
Benji Sloan <sloanrobe@hotmail.com>
2025-10-07 11:57:34 +0200

    Reduced img width to add fifth column; added navbar scroll

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,10 +4,10 @@ import CloseIcon from '@mui/icons-material/Close';
 import Img1 from '../galleryimg/140.jpg';
 import Img2 from '../galleryimg/388.jpg';
 import Img3 from '../galleryimg/390.jpg';
-import Img4 from '../galleryimg/BenjiandVinny.jpg';
+import Img4 from '../galleryimg/JetsBenjiVinny.jpg';
 import Img5 from '../galleryimg/Benji-Jets-Coaches.jpg';
-import Img6 from '../galleryimg/Benji-LeBron.png';
-import Img7 from '../galleryimg/benjimiami.jpg';
+import Img6 from '../galleryimg/JetsRayLucasNews.jpg';
+import Img7 from '../galleryimg/BenjiMiamiHerald.jpg';
 import Img8 from '../galleryimg/Fred-Taylor.jpg';
 import Img9 from '../galleryimg/Marvin-Jones.jpg';
 import Img10 from '../galleryimg/NYJ-Staff-1999.jpg';
@@ -15,7 +15,7 @@ import Img11 from '../galleryimg/Tony-Gonzalez.JPG';
 import Img12 from '../galleryimg/Trainers.jpg';
 import Img13 from '../galleryimg/Trainers3.jpg';
 import Img14 from '../galleryimg/Trainers4.jpg';
-import Img15 from '../galleryimg/Trainers5.jpg';
+import Img15 from '../galleryimg/JetsTeamPhoto.jpg';
 import Img16 from '../galleryimg/Trainers8.jpg';
 import Img17 from '../galleryimg/Venue1.jpg';
 import Img18 from '../galleryimg/Venue2.jpg';
@@ -25,26 +25,26 @@ import Img21 from '../galleryimg/World-Football-Summit.jpg';
 
 const data = [
   { id: 1, imgSrc: Img5, alt: '140' },
-  { id: 2, imgSrc: Img1, alt: '388' },
+  { id: 2, imgSrc: Img6, alt: '388' },
   { id: 3, imgSrc: Img3, alt: '390' },
   { id: 4, imgSrc: Img19, alt: 'Benji and Vinny' },
   { id: 5, imgSrc: Img12, alt: 'Benji Jets Coaches' },
   { id: 6, imgSrc: Img17, alt: 'Benji & LeBron' },
   { id: 7, imgSrc: Img7, alt: 'Benji Miami' },
   { id: 8, imgSrc: Img8, alt: 'Fred Taylor' },
-  { id: 9, imgSrc: Img14, alt: 'Venue 4' },
+  { id: 9, imgSrc: Img15, alt: 'Venue 4' },
   { id: 10, imgSrc: Img11, alt: 'Marvin Jones' },
-  { id: 11, imgSrc: Img10, alt: 'NYJ Staff 1999' },
+  { id: 11, imgSrc: Img21, alt: 'NYJ Staff 1999' },
   { id: 12, imgSrc: Img9, alt: 'Tony Gonzalez' },
-  { id: 13, imgSrc: Img21, alt: 'Trainers' },
-  { id: 14, imgSrc: Img6, alt: 'Trainers 3' },
-  { id: 15, imgSrc: Img4, alt: 'Trainers 4' },
+  { id: 13, imgSrc: Img10, alt: 'Trainers' },
+  { id: 14, imgSrc: Img1, alt: 'Trainers 3' },
+  { id: 15, imgSrc: Img16, alt: 'Trainers 4' },
   { id: 16, imgSrc: Img20, alt: 'Trainers 5' },
-  { id: 17, imgSrc: Img16, alt: 'Trainers 8' },
+  { id: 17, imgSrc: Img4, alt: 'Trainers 8' },
   { id: 18, imgSrc: Img2, alt: 'Venue 1' },
   { id: 19, imgSrc: Img13, alt: 'Venue 2' },
   { id: 20, imgSrc: Img18, alt: 'Venue 3' },
-  { id: 21, imgSrc: Img15, alt: 'World Football Summit' },
+  { id: 21, imgSrc: Img14, alt: 'World Football Summit' },
 ];
 
 function Gallery() {
@@ -81,7 +81,7 @@ function Gallery() {
   }, [model, closeModal]);
 
   return (
-    <>
+    <div id='gallery-nav-scroll'>
       <h1>Gallery</h1>
 
       {/* Modal overlay */}
@@ -125,7 +125,7 @@ function Gallery() {
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
