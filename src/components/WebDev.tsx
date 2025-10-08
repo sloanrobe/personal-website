9573750696fbde4682a98d6a7718d5bf548ac006
Benji Sloan <sloanrobe@hotmail.com>
2025-10-08 17:33:15 +0200

    Fixed indentation error on one line

--- src/components/WebDev.tsx
+++ src/components/WebDev.tsx
@@ -20,7 +20,7 @@ function WebDev() {
         <p>I've built projects from scratch, broken plenty of code along the way, and learned how to fix it (and sometimes even make it better).</p>
         <div className="divider"></div>
         <p className='webdev-question'>So, what exactly does <i>this</i> mean?</p>
-        <p>It means I'm not showing up pretending to be a senior engineer—I'm here to play a solid junior role.</p>
+        <p className='webdev-answer'>It means I'm not showing up pretending to be a senior engineer—I'm here to play a solid junior role.</p>
         <p>I can build features, support a team, and contribute to projects while continuing to sharpen my skills. I know enough to be dangerous in a good way, and I'm committed to learning from those ahead of me.</p>
         <p>I may still be learning, but coding reminds me of my work in sports tech consulting—turning complex data into something usable that actually drives performance. In both worlds, the key is translating ideas into tools that people can trust and use.</p>
         <hr />
