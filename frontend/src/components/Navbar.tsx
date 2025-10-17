400912025dd890e27b93d00e74cae36373f66e26
Benji Sloan <sloanrobe@hotmail.com>
2025-10-17 12:29:51 +0200

    Responsive design implementation

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -39,20 +39,53 @@ function Navbar() {
         <li><a href="#webdev-nav-scroll">Web Development</a></li>
         <li><a href="#testimonials-nav-scroll">Testimonials</a></li>
         <li><a href="#gallery-nav-scroll">Gallery</a></li>
-        {/* <li>Timeline</li> */}
-        {/* <li>Contact</li> */}
       </ul>
       <ContactButton label={'Contact'} />
-      {/* <div className='contact-logos'>
-        <a href="https://www.linkedin.com/in/benji-sloan/">
-          <img className='linkedin-logo' src="public/logos/LinkedIn_logo_initials.png" alt="LinkedIn icon" />
-        </a>
-        <a href="mailto:benji@flounleashed.com">
-          <img className='email-logo' src="public/logos/email-logo.png" alt="Email icon" />
-        </a>
-      </div> */}
     </nav>
   )
 }
 
 export default Navbar
+
+
+
+
+
+
+
+/* import React, { useEffect, useRef, useState } from 'react';
+import ContactButton from './ContactButton';
+import '../styles/contactbutton.css';
+
+function Navbar() {
+  const navRef = useRef<HTMLElement | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className='navbar' ref={navRef}>
+      <img className='signature' src="public/images/Signature2.png" alt="Signature Logo" />
+
+      <button className="menu-toggle" onClick={toggleMenu} aria-label="Toggle navigation menu">
+        <div className={`burger ${isOpen ? 'open' : ''}`}></div>
+        <div className={`burger ${isOpen ? 'open' : ''}`}></div>
+        <div className={`burger ${isOpen ? 'open' : ''}`}></div>
+      </button>
+
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <li><a onClick={toggleMenu} href="#about-nav-scroll">About</a></li>
+        <li><a onClick={toggleMenu} href="#experience-nav-scroll">Experience</a></li>
+        <li><a onClick={toggleMenu} href="#webdev-nav-scroll">Web Development</a></li>
+        <li><a onClick={toggleMenu} href="#testimonials-nav-scroll">Testimonials</a></li>
+        <li><a onClick={toggleMenu} href="#gallery-nav-scroll">Gallery</a></li>
+      </ul>
+      
+      <ContactButton label={'Contact'} />
+    </nav>
+  );
+}
+
+export default Navbar; */
